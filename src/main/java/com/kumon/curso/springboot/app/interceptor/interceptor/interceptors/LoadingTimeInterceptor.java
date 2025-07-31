53f0e7d12612ad4fc82b279a440f2c06fbd9a6f0
Cristian Ortega <cristian123.cuop@gmail.com>
2025-07-31 15:11:00 -0600

    Obtener la interaccion del interceptor

--- src/main/java/com/kumon/curso/springboot/app/interceptor/interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/kumon/curso/springboot/app/interceptor/interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.kumon.curso.springboot.app.interceptor.interceptor.interceptors;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
@@ -17,21 +19,36 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            @Nullable ModelAndView modelAndView) throws Exception {
-
-        logger.info("LoadingTimeInterceptor: postHandle saliendo..." + ((HandlerMethod) handler).getMethod().getName());
-
-        return;
-    }
-
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
 
         HandlerMethod controller = ((HandlerMethod) handler);
         logger.info("LoadingTimeInterceptor: preHandle entrando..." + controller.getMethod().getName());
+
+        long start = System.currentTimeMillis();
+        request.setAttribute("start", start);
+
+        Random random = new Random();
+        int delay = random.nextInt(500);
+
+        Thread.sleep(delay);
+
         return true;
     }
 
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+
+        logger.info("LoadingTimeInterceptor: postHandle saliendo..." + ((HandlerMethod) handler).getMethod().getName());
+
+        long end = System.currentTimeMillis();
+
+        long start = (long) request.getAttribute("start");
+
+        long result = end - start;
+
+        logger.info("Tiempo trancurrido..." + result);
+        return;
+    }
 }
